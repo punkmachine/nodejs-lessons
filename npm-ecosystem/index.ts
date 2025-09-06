137188622c6150c6c021210c2025033752ed075a
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2025-09-06 14:06:46 +0500

    save lesson 1

--- npm-ecosystem/index.ts
+++ npm-ecosystem/index.ts
@@ -1,15 +1,25 @@
 import http from 'http';
 import { IncomingMessage, ServerResponse } from 'http';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Код запущен в режиме разработки');
+}
+
 const server = http.createServer((request: IncomingMessage, response: ServerResponse) => {
-  response.statusCode = 200;
-  response.statusMessage = 'OK';
-  response.setHeader('Content-Type', 'text/plain');
+  // response.statusCode = 200;
+  // response.statusMessage = 'OK';
+  // response.setHeader('Content-Type', 'text/plain');
 
-  response.write('Hello, ');
-  response.write('world!');
+  // response.write('Hello, ');
+  // response.write('world!');
 
-  response.end();
+  // response.end();
+
+  response.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf8'
+  });
+
+  response.end('<h1>Привет, мир!</h1>', 'utf8');
 });
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
